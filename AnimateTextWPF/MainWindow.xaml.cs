cfbdf2d84af3547e359c2075b63b6209cdc7b88b
Steeve Santiago <HCC756@Belgrid.net>
2024-05-15 09:46:55 +0200

    Add project files.

---
+++ AnimateTextWPF/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace AnimateTextWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowVM();
+        }
+    }
+}
